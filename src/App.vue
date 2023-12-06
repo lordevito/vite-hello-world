9742f58140d3ea1ac15e544167ef6f0ab64d0a55
lordevito <lollo.devito@hotmail.com>
2023-12-06 20:17:10 +0100

    feat: esercizio svolto

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import AppTitle from './components/AppTitle.vue';
+export default {
+  data() {
+    return {
+      title: 'E funziona pure! Magico!',
+    };
+  },
+  components:{
+    AppTitle,
+  }
+};
+</script>
+
+<template>
+  <AppTitle />
+  <h2>{{ title }}</h2>
+</template>
+
+<style scoped>
+
+</style>
